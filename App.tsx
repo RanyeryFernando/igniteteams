4912491359bb15ce1a64aa7f7c5201491f0ecfb2
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-02 21:08:26 -0300

    definindo o tema

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+import Theme  from 'src/theme'
 import { Groups } from '@screens/Groups';
 
 export default function App() {
   return (
+    <ThemeProvider theme={Theme}>
     <Groups />
+    </ThemeProvider>
   );
 }
