63fd249e29f7ce2c6847bc9ed83341b3f7d19daf
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-04 10:23:12 -0300

    Utilizando tema

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { ThemeProvider } from 'styled-components'
-import Theme  from 'src/theme'
+import { ThemeProvider } from 'styled-components';
+import Theme  from './src/theme/index';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
