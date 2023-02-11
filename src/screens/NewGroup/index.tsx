8af1cdeca07b527489a9cbc4207def4bbbac4925
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-11 17:03:11 -0300

    Adicionando componente input

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup(){
@@ -13,8 +14,10 @@ export function NewGroup(){
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pesssoas"
         />
+        <Input />
         <Button 
           title="Criar"
+          style={{ marginTop: 20 }}
         />
       </Content>
     </Container>
