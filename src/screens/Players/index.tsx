a43fa379266d9f3fd5511339d68bc11b8af6d9f9
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-20 09:08:06 -0300

    Criando componente Button Icon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,4 +1,6 @@
 import { Header } from "@components/Header";
+import { Input } from "@components/input";
+import { ButtonIcon } from "@components/Buttonicon";
 import { Highlight } from "@components/Highlight";
 import { Container } from "./styles";
 
@@ -10,6 +12,13 @@ export function Players(){
       <Highlight 
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
+      />
+      <Input 
+        placeholder="Nome da pessoa"
+        autoCorrect={false}
+      />
+      <ButtonIcon 
+        icon="add"
       />
     </Container>
   );
