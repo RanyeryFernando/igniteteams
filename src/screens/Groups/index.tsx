a2ad61664bd6c5790e5b1d1f62fa2023e6d9c844
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-21 15:22:50 -0300

    navegando pela aplicacao

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -15,7 +15,7 @@ export function Groups() {
   const navigation = useNavigation();
 
   function handleNewGroup(){
-    navigation.navigate('groups');
+    navigation.navigate('new');
   }
 
   return (
