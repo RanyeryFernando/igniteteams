9c74b1701f936c2aa0d188188daa17158535c177
Ranyery Fernando <83469345+RanyeryFernando@users.noreply.github.com>
2023-02-21 11:28:34 -0300

    Listando os filter v2

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
+import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Input } from "@components/input";
 import { Filter } from "@components/Filter";
 import { ButtonIcon } from "@components/Buttonicon";
 import { Highlight } from "@components/Highlight";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
+import { useState } from "react";
 
 export function Players(){
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return(
     <Container>
       <Header showBackButton />
@@ -25,10 +30,24 @@ export function Players(){
       />
     </Form>
 
-    <Filter 
-      title="Time A"
-      isActive
-    />
+    <HeaderList>
+      <FlatList 
+        data={['Time A', 'Time B']}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Filter 
+            title={item}
+            isActive={item === team}
+            onPress={() => setTeam(item)}
+          />
+        )}
+        horizontal
+      />
+      <NumbersOfPlayers>
+        {players.length}
+      </NumbersOfPlayers>
+    </HeaderList>
+
     </Container>
   );
 }
